dd54693a570730ab883c612cd46f468b8d199a98
Jayesha Dampage <88516562+JDampage@users.noreply.github.com>
2021-08-30 13:38:04 +0530

    Update channeladmins.py

--- DaisyXMusic/modules/channeladmins.py
+++ DaisyXMusic/modules/channeladmins.py
@@ -148,4 +148,4 @@ async def admincache(client, message: Message):
             for member in await conchat.linked_chat.get_members(filter="administrators")
         ],
     )
-    await message.reply_text("❇️ Admin cache refreshed!")
+    await message.reply_text("⚜️ Admin cache refreshed!")
